fe86aafd5f9b04a331fbddc03a1614499757b8e6
Brady Robshaw <bradyr8@vt.edu>
2022-01-01 15:12:03 -0500

    add serving size piker and funshanalaty

--- recipe_book/ViewModel/RecipeModel.swift
+++ recipe_book/ViewModel/RecipeModel.swift
@@ -15,4 +15,65 @@ class RecipeModel: ObservableObject {
         self.recipe = jsonUnpack.unpcack()
     }
     
+    static func getPortion(ingredient:Ingredient, recipeServings:Int, targetServings:Int) -> String {
+        
+        var portion = ""
+        var numerator = ingredient.num ?? 1
+        var denominator = ingredient.denom ?? 1
+        var wholePortion = 0
+         
+        
+        if ingredient.num != nil {
+            
+            //get a singal serving size by mutaplaing denomanator by the  recapy servings
+            denominator *= recipeServings
+            
+            //get target portion by mutaplying the numeratoer by the target servings
+            numerator *= targetServings
+            
+            //redce the fration by gratust common divisor
+            let divisor = Rational.greatestCommonDivisor(numerator, denominator)
+            numerator /= divisor
+            denominator /= divisor
+            //get the whole portin if the nuarator > denomonator
+            if numerator >= denominator {
+                wholePortion = numerator / denominator
+                numerator = numerator % denominator
+                portion += String(wholePortion)
+            }
+            
+            //Expres the remandor as a fration
+            if numerator > 0 {
+                portion += wholePortion > 0 ? " " : ""
+                portion += "\(numerator)/\(denominator)"
+            }
+            
+        }
+        
+        if var unit = ingredient.unit {
+            
+            //do we nead to mack plerl
+            if wholePortion > 1 {
+                
+                if unit.suffix(2) == "ch" {
+                    unit += "es"
+                }
+                else if unit.suffix(1) == "f" {
+                    unit = String(unit.dropLast())
+                    unit += "ves"
+                }
+                else {
+                    unit += "s"
+                }
+                
+            }
+            
+            portion += ingredient.num == nil ? "" : " "
+            
+            return portion + unit
+        }
+        
+        return String(portion)
+    }
+    
 }
